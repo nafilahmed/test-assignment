50d5d3f686c8f225e965308a92f33a3286f979ff
Nafil Ahmed <nafil.ahmed@arpatech.com>
2023-11-23 23:47:51 +0500

    Hiding Wordpress Users

--- resources/js/components/Candidates.vue
+++ resources/js/components/Candidates.vue
@@ -4,7 +4,7 @@
       <h1 class="text-4xl font-bold">Candidates</h1>
     </div>
     <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
-      <div v-for="candidate in candidates" class="rounded overflow-hidden shadow-lg">
+      <div v-for="candidate in candidates" class="rounded overflow-hidden shadow-lg" v-show="Object.values(JSON.parse(candidate.strengths)).indexOf('Wordpress') < 0">
         <img class="w-full" src="/avatar.png" alt="">
         <div class="px-6 py-4">
           <div class="font-bold text-xl mb-2">{{ candidate.name }}</div>
@@ -15,7 +15,7 @@
               strength }}</span>
         </div>
         <div class="px-6 pb-2"><span v-for="skill in JSON.parse(candidate.soft_skills)"
-            class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{{ skill
+            class="inline-block rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2" :class="[desiredSoftSkills.includes(skill) ? 'bg-green-200' : 'bg-gray-200']">{{ skill
             }}</span>
         </div>
         <div class="p-6 float-right">
@@ -49,6 +49,9 @@ export default {
     return {
       desiredStrengths: [
         'Vue.js', 'Laravel', 'PHP', 'TailwindCSS'
+      ],
+      desiredSoftSkills: [
+        'Team player', 'Leadership', 'Conflict management', 'Diplomacy'
       ],
       isLoading: false,
     }
